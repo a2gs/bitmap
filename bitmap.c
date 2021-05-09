033089c478c949fecb69516e5aa7a1c6789f92aa
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2021-05-09 20:36:54 -0300

    build bitmap

--- bitmap.c
+++ bitmap.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define BT_OK (1)
+#define BT_OK    (1)
 #define BT_ERROR (0)
 
+/* https://github.com/a2gs/bitwiseByteGetSetBits */
 #define GETBIT(__byte_, __n_) ((__byte_ & (((unsigned char) 1) << (CHAR_BIT - __n_))) ? 1 : 0)
+#define SETBIT(__byte_, __n_) (__byte_ |= ((unsigned char) 1) << (CHAR_BIT - __n_))
 
 int bitmapProc(unsigned char *bitmap, size_t bitmapsize)
 {
@@ -29,12 +31,39 @@ int bitmapProc(unsigned char *bitmap, size_t bitmapsize)
 	return(BT_OK);
 }
 
+int bitmapSet(unsigned char *bitmap, size_t bitmapsize, unsigned int bit)
+{
+	unsigned int offset = 0;
+
+	if(bit >= (unsigned int)bitmapsize)
+		return(BT_ERROR);
+
+	offset = (unsigned int)(bit / CHAR_BIT);
+
+	/* newBit = (CHAR_BIT - (((offset + 1) * CHAR_BIT) - bit )) + 1; */
+
+	SETBIT(bitmap[offset], ((CHAR_BIT + 1) - (offset * CHAR_BIT + CHAR_BIT - bit)));
+
+	return(BT_OK);
+}
+
 
 int main(int argc, char *argv[])
 {
 #define BITMAPSIZE (16)
 	size_t bitmapsize = 40;
-	unsigned char bitmap[BITMAPSIZE] = {"\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA"};
+	unsigned char bitmap[BITMAPSIZE] = {0};
+
+	bitmapSet(bitmap, bitmapsize, 1);
+	bitmapSet(bitmap, bitmapsize, 40);
+	bitmapSet(bitmap, bitmapsize, 7);
+	bitmapSet(bitmap, bitmapsize, 26);
+	bitmapSet(bitmap, bitmapsize, 8);
+	bitmapSet(bitmap, bitmapsize, 13);
+	bitmapSet(bitmap, bitmapsize, 3);
+	bitmapSet(bitmap, bitmapsize, 14);
+	bitmapSet(bitmap, bitmapsize, 0);
+	bitmapSet(bitmap, bitmapsize, 31);
 
 	if(bitmapProc(bitmap, bitmapsize) == BT_ERROR){
 		return(-1);
